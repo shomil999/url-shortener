187301b2c4f741d55879a5b1dbfda5789ba1244a
Shomil <shomilmaurya99@gmail.com>
2025-11-01 17:20:16 +0530

    added in memory storage layer

--- internal/store/memory.go
+++ internal/store/memory.go
@@ -1,1 +1,52 @@
-// in memory store file
+package store
+
+import (
+	"errors"
+	"sync"
+)
+
+var errNotFound = errors.New("not found")
+
+type MemoryStore struct {
+	mu        sync.RWMutex
+	urlToCode map[string]string
+	codeToURL map[string]string
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		urlToCode: make(map[string]string),
+		codeToURL: make(map[string]string),
+	}
+}
+
+func (s *MemoryStore) GetByURL(url string) (string, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	code, ok := s.urlToCode[url]
+	if !ok {
+		return "", errNotFound
+	}
+	return code, nil
+}
+
+func (s *MemoryStore) GetByCode(code string) (string, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	url, ok := s.codeToURL[code]
+	if !ok {
+		return "", errNotFound
+	}
+	return url, nil
+}
+
+func (s *MemoryStore) Save(url, code string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, ok := s.urlToCode[url]; ok {
+		return nil
+	}
+	s.urlToCode[url] = code
+	s.codeToURL[code] = url
+	return nil
+}
