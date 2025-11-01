9b9cd383d52280469920e8432835f4d16ea5b70f
Shomil <shomilmaurya99@gmail.com>
2025-11-01 17:32:13 +0530

    added service layer

--- internal/shortener/service.go
+++ internal/shortener/service.go
@@ -1,1 +1,22 @@
-// service file
+package shortener
+
+import (
+	"crypto/sha256"
+	"errors"
+	"net/url"
+	"strings"
+
+	"github.com/shomil999/url-shortener/internal/metrics"
+	"github.com/shomil999/url-shortener/internal/store"
+	"github.com/shomil999/url-shortener/pkg/util"
+)
+
+type Service struct {
+	store   *store.MemoryStore
+	metrics *metrics.Metrics
+	baseURL string
+}
+
+func NewService(st *store.MemoryStore, m *metrics.Metrics, baseURL string) *Service {
+	return &Service{store: st, metrics: m, baseURL: strings.TrimRight(baseURL, "/")}
+}
