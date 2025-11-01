a9fb75daff57c02556b460805ea7132aea470d31
Shomil <shomilmaurya99@gmail.com>
2025-11-01 17:54:58 +0530

    created initial handler template class

--- internal/httpapi/handlers.go
+++ internal/httpapi/handlers.go
@@ -1,1 +1,20 @@
-// This is api handler file
+package httpapi
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/shomil999/url-shortener/internal/metrics"
+	"github.com/shomil999/url-shortener/internal/shortener"
+)
+
+type Handlers struct {
+	svc *shortener.Service
+	met *metrics.Metrics
+}
+
+func NewHandlers(svc *shortener.Service, met *metrics.Metrics) *Handlers {
+	return &Handlers{svc: svc, met: met}
+}
